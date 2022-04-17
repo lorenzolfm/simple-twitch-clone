31bfbb96067dd35491360fc045bc97b592b1a420
Lorenzo Maturano <maturanolorenzo@gmail.com>
2022-04-17 19:08:57 -0300

    feat: hooking up redux form

--- src/components/streams/StreamCreate.tsx
+++ src/components/streams/StreamCreate.tsx
@@ -1,5 +1,24 @@
-export const StreamCreate = () => {
+import { Field, InjectedFormProps, reduxForm, WrappedFieldProps } from 'redux-form';
+
+//const _StreamCreate = (props: InjectedFormProps<Record<string, never>>) => {
+const _StreamCreate = (): JSX.Element => {
+  const renderInput = ({ input, label }: WrappedFieldProps & { label: string }) => {
+    return (
+      <div className="field">
+        <label htmlFor={input.name}>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  };
+
   return (
-    <div>StreamCreate</div>
+    <form className="ui form">
+      <Field name="title" component={renderInput} label="Enter Title" />
+      <Field name="description" component={renderInput} label="Enter Description" />
+    </form>
   );
 };
+
+export const StreamCreate = reduxForm<Record<string, never>>({
+  form: 'streamCreate',
+})(_StreamCreate);
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,11 +1,14 @@
 import { combineReducers } from 'redux';
+import { FormStateMap, reducer as formReducer } from 'redux-form';
 
 import { Auth, authReducer } from './authReducer';
 
 export interface StoreState {
   auth: Auth;
+  form: FormStateMap;
 }
 
 export const reducers = combineReducers<StoreState>({
   auth: authReducer,
+  form: formReducer,
 });
