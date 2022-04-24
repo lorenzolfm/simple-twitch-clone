313b1be3ddb51eac014511fb7875ffdf504abc39
Lorenzo Maturano <maturanolorenzo@gmail.com>
2022-04-24 18:09:47 -0300

    feat: fetching stream

--- src/components/streams/StreamEdit.tsx
+++ src/components/streams/StreamEdit.tsx
@@ -1,5 +1,38 @@
-export const StreamEdit = () => {
-  return (
-    <div>StreamEdit</div>
-  );
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
+import { StoreState } from '../../reducers';
+import { Stream } from '../../types';
+
+type Props = {
+  match: { params: { id: number } };
+  stream: Stream
+  fetchStream: (id: number) => Promise<void>;
+}
+
+export const _StreamEdit = (
+  {
+    stream,
+    fetchStream,
+    match: { params: { id } },
+  }: Props,
+) => {
+  useEffect(() => {
+    fetchStream(id);
+  }, []);
+
+  if (!stream)
+    return <div>Loading</div>;
+
+  return <div>{stream.title}</div>;
 };
+
+const mapStateToProps = (
+  { streams }: StoreState,
+  { match: { params: { id } } }: Props,
+) => {
+  return { stream: streams[id] };
+};
+
+export const StreamEdit =
+  connect(mapStateToProps, { fetchStream })(_StreamEdit);
