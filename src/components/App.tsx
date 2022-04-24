e60f07f7323a1c82886a64f02ecb2dca8fab0f2e
Lorenzo Maturano <maturanolorenzo@gmail.com>
2022-04-24 18:09:20 -0300

    feat: using history on router and wildcard navigation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router as BrowserRouter, Route } from 'react-router-dom';
+import history from '../history';
 
 import { Header } from './Header';
 import {
@@ -12,12 +13,12 @@ import {
 export const App = (): JSX.Element => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <BrowserRouter history={history}>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </div>
